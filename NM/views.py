fe14068e997411ae961aa4e8743abebfea2d2479
oyeshiv <shivtiwari1609@gmail.com>
2021-10-29 11:02:55 +1030

    ..

--- NM/views.py
+++ NM/views.py
@@ -1,13 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from NM.forms import ISR4321From
-
+from django.contrib.auth.models import User, Group
 from NM.models import ISR4321, Devices, Projects, Scripts
 # Create views
 
+
 def dash(request):
-    projects = Projects.objects.all()
-    return render(request, 'projects.html', {'projects': projects})
+    
+    user = User.objects.get(username=request.POST['username'])
+    all_group = user.groups.all()
+    user_group = 0
+    organisation_name = ""
+    for osn in all_group:
+        user_group = osn.id
+        organisation_name = osn.name.upper()
+    if user_group == 2 or request.POST['username']=='shivam':
+        return redirect('admin/')
+    projects = Projects.objects.filter(organisation_id=user_group)
+    return render(request, 'projects.html', {'projects': projects, 'user':user, 'organisation_name':organisation_name})
+    
+
+
+def login_user(request):
+
+    result = render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pass']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            result = dash(request)
+        else:
+            messages.success(request, ('Incorrect User Credentials!'))
+            result = render(request, 'login.html')
+    return result
 
 def scripts(request):
     result_scripts = Scripts.objects.filter(project_id=request.POST['id']).select_related('device')
@@ -28,14 +58,6 @@ def form(request):
     form = ISR4321From(request.POST)
 
     return render(request, 'form.html', {'form':form}) 
-
-def login(request):
-
-    if request.method == 'POST':
-        print()
-
-    else:
-        return render(request, 'login.html')
 
 def base(request):
     return render(request, 'base.html')
