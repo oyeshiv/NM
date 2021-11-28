5029c1a05c1a1bbfa0af831eb759be0aba8e8ed6
oyeshiv <shivtiwari1609@gmail.com>
2021-11-29 06:53:37 +1030

    ..

--- NM/settings.py
+++ NM/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 
 # use this in development
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_FINDERS = (
   'django.contrib.staticfiles.finders.FileSystemFinder',
