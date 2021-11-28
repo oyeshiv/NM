61cb40f34439d6a58697aa81b897c24787233acb
oyeshiv <shivtiwari1609@gmail.com>
2021-11-29 06:31:59 +1030

    ..

--- NM/settings.py
+++ NM/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k1*)7lg+!4cbl8d(q#g#uba_r9^a4kpw!al$!#d(y6g3h)y7e!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
 
